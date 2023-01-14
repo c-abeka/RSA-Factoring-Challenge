fc3b296a2002f6cc5801acfa1b2fdec818f533db
c-abeka <havelstech@gmail.com>
2023-01-14 13:42:48 +0300

    factoring challenge

---
+++ factors.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+''' This is a Factorization Challenge '''
+
+
+def factorize(n):
+    '''
+        factorize(n)
+        ===========
+        Computes the factors of a number n
+        ---------------------------------
+    '''
+    if type(n) is not int:
+        raise TypeError('n must be a positive integer')
+
+    for d in range(2, 10):
+        if n % d == 0:
+            print("{}={}*{}".format(n, n // d, d))
+            return
+
+    sqrn = n ** 0.5 #get the square root of n
+    prime = 11;
+
+    while prime < sqrn:
+        if n % prime == 0:
+            print("{}={}*{}".format(n, n // prime, prime))
+            return
+        else:
+            prime += 2
+    
+    print("{}={}*{}".format(n, n , 1))
+    return
+    
+            
+        
+'''
+    for div in range(2, 10):
+        
+        try:
+            if n % div == 0 and div != 1:
+                print("{}={}*{}".format(n, n / div, div))
+                break
+            elif div in range(11, n/2 + 1):
+                if n % div == 0:
+                    print("{}={}*{}".format(n, n / div, div))
+                    break 
+        
+        except Exception:
+            for i in range(3, round(n ** 0.5), 2):
+                if n % i == 0:
+                    print("{}={}*{}".format(n, n / i, i))
+
+    if __name__ == "__main__":
+        import doctest
+        import sys
+    
+        f = sys.argv[1]
+        doctest.testfile(f)
+'''
